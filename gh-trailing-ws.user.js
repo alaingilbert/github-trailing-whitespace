df65b872bd5887a20f2dc8e33b493beb6b4b1d1d
Alain Gilbert <alain.gilbert.15@gmail.com>
2016-06-30 16:31:24 -0700

    Strong highlight of trailing whitespaces

--- gh-trailing-ws.user.js
+++ gh-trailing-ws.user.js
@@ -2,9 +2,11 @@ lines = document.getElementsByClassName('blob-code-inner');
 for(var i=0; i<lines.length; i++) {
     var line = lines[i];
     var lastChild = line.childNodes[line.childNodes.length-1];
-    if(lastChild.nodeType == lastChild.TEXT_NODE && /^\s+$/.test(lastChild.textContent)) {
+    var childCount = line.childNodes.length;
+    if(childCount > 1 && lastChild.nodeType == lastChild.TEXT_NODE && /^\s+$/.test(lastChild.textContent)) {
         var span = document.createElement('span');
         span.className = 'x';
+        span.style.backgroundColor = 'red';
         span.innerText = lastChild.textContent;
         line.insertBefore(span, lastChild);
         line.removeChild(lastChild);
